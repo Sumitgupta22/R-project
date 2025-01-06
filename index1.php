df030dcf8168de867a5b7695676548e3dcf4adcf
sumitgupta22 <sumitgupta969139@gmail.com>
2025-01-06 15:10:17 +0530

    d1

---
+++ index1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="bootstrap.css" />
+    <link rel="stylesheet" href="style1.css" />
+    <title>RECORD CREATER</title>
+  </head>
+
+  <body>
+    <div class="head">
+      <div class="container1">
+        <div class="container">
+          <div class="row">
+            <div class="col"><div class="col">MAKE WORK BETTER LIFE</div></div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <a href="Your facebook link"
+              ><img
+                src="image/instagram.svg"
+                alt=""
+                style="height: 20px; width: 20px"
+            /></a>
+          </div>
+          <div class="col">
+            <img
+              src="image/logo.jpeg"
+              alt=""
+              style="height: 100px; width: 100px"
+            />
+          </div>
+          <div class="col"></div>
+        </div>
+      </div>
+    </div>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
